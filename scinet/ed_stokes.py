96e8577e435bd9d7e3ff5e6c535de16a3934b3d8
Jacob <jrwest@stanford.edu>
2021-02-08 15:47:23 -0800

    initial implementation and demo of stokes particle motion example

--- scinet/ed_stokes.py
+++ scinet/ed_stokes.py
@@ -17,33 +17,39 @@ import cPickle
 import gzip
 import io
 
+def stokes_eqn(Fr, St, t, x0=0.0, v0=0.0):
+    #solution to equation x'' + 1/St*x' + Fr = 0
+    t = np.array(t)
+    C1 = -Fr*St - v0
+    C2 = x0 - C1*St
+    return np.real( -Fr*St * t + C1*St * np.exp(-t/St) + C2 )
 
-def osc_eqn(A_0, delta_0, b, kappa, t):
-    return np.real(A_0 * np.exp(-b / 2. * t) * np.exp(1 / 2. * np.sqrt(b**2 - 4 * kappa + 0.j) * t + 1.j * delta_0))
+def stokes_data(N, t_sample=np.linspace(0, 5, 50), Fr_interval=[-5, 5], St_interval=[0,5],
+                    t_meas_interval=None, fileName=None):
 
-
-def oscillator_data(N, t_sample=np.linspace(0, 5, 50), A_interval=[1, 1], delta_interval=[0, 0],
-                    b_interval=[0.5, 1], kappa_interval=[5, 10], t_meas_interval=None, fileName=None):
     t_sample = np.array(t_sample, dtype=float)
-    # cover edges
-    b_interval = [0.8 * b_interval[0], 1.2 * b_interval[1]]
-    kappa_interval = [0.8 * kappa_interval[0], 1.2 * kappa_interval[1]]
-    bb = (b_interval[1] - b_interval[0]) * np.random.rand(N) + b_interval[0]
-    kk = (kappa_interval[1] - kappa_interval[0]) * np.random.rand(N) + kappa_interval[0]
-    AA = (A_interval[1] - A_interval[0]) * np.random.rand(N) + A_interval[0]
-    dd = (delta_interval[1] - delta_interval[0]) * np.random.rand(N) + delta_interval[0]
-    x_in = []
+
+    # sample in intervals
+    Fr_all = (Fr_interval[1] - Fr_interval[0]) * np.random.rand(N) + Fr_interval[0]
+    St_all = (St_interval[1] - St_interval[0]) * np.random.rand(N) + St_interval[0]
+
+    #If measurement time not provided, create it to extrapolate
     if t_meas_interval is None:
         t_meas_interval = [t_sample[0], 2 * t_sample[-1]]
     t_meas = np.reshape(np.random.rand(N) * (t_meas_interval[1] - t_meas_interval[0]) + t_meas_interval[0], [N, 1])
+
+    x_in = []
     x_out = []
-    for b, kappa, A_0, delta_0, t in zip(bb, kk, AA, dd, t_meas):
-        x_in.append(osc_eqn(A_0, delta_0, b, kappa, t_sample))
-        x_out.append(osc_eqn(A_0, delta_0, b, kappa, t))
+
+    for Fr, St, t in zip(Fr_all, St_all, t_meas):
+        x_in.append(  stokes_eqn(Fr, St, t_sample ) ) 
+        x_out.append( stokes_eqn(Fr, St, t        ) ) 
+
     x_in = np.array(x_in)
     x_out = np.reshape(x_out, [N, 1])
-    state_list = np.vstack([AA, dd, bb, kk]).T
+    state_list = np.vstack([Fr_all, St_all]).T
     result = ([x_in, t_meas, x_out], state_list, [])
+
     if fileName is not None:
         f = gzip.open(io.data_path + fileName + ".plk.gz", 'wb')
         cPickle.dump(result, f, protocol=2)
